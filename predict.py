e7a56f4cda29bba92ac9b0b1cc9e70c7de31f8e0
moonlight <arqzhou@gmail.com>
2026-07-02 12:06:02 -0500

    Cleaned up data set, prepared to split

--- predict.py
+++ predict.py
@@ -4,19 +4,13 @@ import pandas as pd
 # Download latest version
 path = kagglehub.dataset_download("nathanlauga/nba-games")
 
-print("Path to dataset files:", path)
+# print("Path to dataset files:", path)
 
 games_raw = pd.read_csv(f"{path}/games.csv")
-games = games_raw[['GAME_DATE_EST', 'HOME_TEAM_ID', 'VISITOR_TEAM_ID']]
-games.info()
+games = games_raw[['GAME_DATE_EST', 'HOME_TEAM_ID', 'VISITOR_TEAM_ID', 'HOME_TEAM_WINS']]
 
 rankings_raw = pd.read_csv(f"{path}/ranking.csv")
 rankings = rankings_raw[['TEAM_ID', 'STANDINGSDATE', 'W_PCT', 'HOME_RECORD', 'ROAD_RECORD', 'W', 'L']]
-
-print(rankings_raw.columns)
-print(rankings.iloc[:3])
-
-print(games.iloc[:3])
 
 home_merged = pd.merge(
     games,
@@ -35,3 +29,24 @@ total_merged = pd.merge(
 ).drop(columns=['TEAM_ID_away', 'STANDINGSDATE_away'])
 
 print(total_merged.head())
+
+def convert_to_percentage(df, colname, isHomeTeam):
+    suffix = '_home' if isHomeTeam else '_away'
+    col, col_w, col_l = (colname + suffix), (colname + suffix + '_W'), (colname + suffix + '_L')
+    df[[col_w, col_l]] = df[col].str.split('-', expand=True)
+    df = df.astype({col_w: int, col_l: int})
+    df[col] = round(df[col_w] / (df[col_w] + df[col_l]), 3)
+    df = df.drop(columns=[col_w, col_l])
+    return(df)
+
+def convert_to_percentages(data):
+    df = convert_to_percentage(data, 'HOME_RECORD', True)
+    df = convert_to_percentage(df, 'HOME_RECORD', False)
+    df = convert_to_percentage(df, 'ROAD_RECORD', True)
+    df = convert_to_percentage(df, 'ROAD_RECORD', False)
+    return(df)
+    
+total_merged = convert_to_percentages(total_merged)
+print(total_merged.head())
+print(total_merged.shape)
+    
