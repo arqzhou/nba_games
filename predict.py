cd013235594e82939f5b8ba3bac71db7ebe205bb
moonlight <arqzhou@gmail.com>
2026-07-03 10:08:21 -0500

    Added DecisionTreeClassifier to test

--- predict.py
+++ predict.py
@@ -3,11 +3,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.tree import DecisionTreeClassifier
+
 
 # Download latest version
 path = kagglehub.dataset_download("nathanlauga/nba-games")
-
-# print("Path to dataset files:", path)
 
 games_raw = pd.read_csv(f"{path}/games.csv")
 games = games_raw[['GAME_DATE_EST', 'HOME_TEAM_ID', 'VISITOR_TEAM_ID', 'HOME_TEAM_WINS']]
@@ -51,32 +51,53 @@ def convert_to_percentages(data):
     return(df)
     
 total_merged = convert_to_percentages(total_merged)[::-1] # flipped to chronological order
-# print(total_merged.head())
-# print(total_merged.shape)
 
-# Independent (predictors) and Dependent (won?) Columns
-X = total_merged[['W_PCT_home', 'W_PCT_away', 'HOME_RECORD_home', 'ROAD_RECORD_away']]
-y = total_merged['HOME_TEAM_WINS']
+def basic_logistic_regression(total_merged):
+    # Independent (predictors) and Dependent (won?) Columns
+    X = total_merged[['W_PCT_home', 'W_PCT_away', 'HOME_RECORD_home', 'ROAD_RECORD_away']]
+    y = total_merged['HOME_TEAM_WINS']
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
-    test_size = 0.2,
-    random_state = 38,
-    shuffle = False,
-    stratify = None
-)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size = 0.2,
+        random_state = 38,
+        shuffle = False,
+        stratify = None
+    )
 
-# print(X_train, y_train)
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    preds = model.predict(X_test)
 
-model = LogisticRegression()
-model.fit(X_train, y_train)
-preds = model.predict(X_test)
+    print(confusion_matrix(y_test, preds))
+    print(classification_report(y_test, preds))
 
-print(X_test)
-print(preds)
-print(y_test)
+    for feature, coef in zip(X.columns, model.coef_[0]):
+        print(f"{feature}: {coef:.3f}")
 
-print(y_train.value_counts(normalize=True))
-print(confusion_matrix(y_test, preds))
-print(classification_report(y_test, preds))
+def decision_tree(total_merged):
+    # Independent (predictors) and Dependent (won?) Columns
+    X = total_merged[['W_PCT_home', 'W_PCT_away', 'HOME_RECORD_home', 'ROAD_RECORD_away']]
+    y = total_merged['HOME_TEAM_WINS']
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size = 0.2,
+        random_state = 38,
+        shuffle = False,
+        stratify = None
+    )
+
+    # Not as effective since there aren't complex interactions.
+    tree = DecisionTreeClassifier(max_depth=4)
+    tree.fit(X_train, y_train)
+    tree_preds = tree.predict(X_test)
+
+    print(confusion_matrix(y_test, tree_preds))
+    print(classification_report(y_test, tree_preds))
+
+
+# basic_logistic_regression(total_merged)
+decision_tree(total_merged)
