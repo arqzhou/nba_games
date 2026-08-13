631261730792154e48398c782471298b0918f3b2
moonlight <arqzhou@gmail.com>
2026-08-13 13:02:40 -0500

    Cleaned up code, added variables to adjust

--- last_x.py
+++ last_x.py
@@ -13,26 +13,17 @@ class RDF:
 
         self._team_games = pd.concat([self._home, self._away]).sort_values('GAME_DATE_EST')
         self._team_games = self._team_games.sort_values(by=['GAME_DATE_EST', 'GAME_ID'], ascending=[True, True]).reset_index(drop=True)
-        # print("Unique Index?", self._team_games.index.is_unique)
-        # print(self._team_games.head())
 
-        # print(type(self._team_games))
-        # print(self._team_games.shape)
-
-
-    # Calculates all the last ten win pcts to create a 'LAST_TEN' variable.
-    def calc_last_tens(self):
+    # Calculates all the last X win pcts to create a 'LAST_X' variable.
+    def calc_last_xs(self, max_window_size):
         rolled = (
             self._team_games.groupby('TEAM_ID')['TEAM_WIN']
-            .rolling(window = 10, min_periods = 1)
+            .rolling(window = max_window_size, min_periods = 1) # adjust X here
             .mean()
         )
         shifted = rolled.groupby(level=0).shift(1)
-
-        self._team_games['LAST_TEN'] = shifted.reset_index(level=0, drop=True)
-
-
-        self._team_games['LAST_TEN'] = self._team_games['LAST_TEN'].fillna(0.5)
+        self._team_games['LAST_X'] = shifted.reset_index(level=0, drop=True)
+        self._team_games['LAST_X'] = self._team_games['LAST_X'].fillna(0.5)
 
         # maybe we could have an agent go thru and label which of these were preseason etc.
 
--- predict.py
+++ predict.py
@@ -4,14 +4,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.tree import DecisionTreeClassifier
-from rolled import RDF
-import mlflow
+from sklearn.ensemble import RandomForestClassifier
+from last_x import RDF
 
 # Download latest version
 path = kagglehub.dataset_download("nathanlauga/nba-games")
-
-mlflow.set_experiment('NBA_predictor')
-mlflow.sklearn.autolog()
 
 # Pull data from two databases
 games_raw = pd.read_csv(f"{path}/games.csv")
@@ -19,6 +16,11 @@ games = games_raw[['GAME_ID', 'GAME_DATE_EST', 'HOME_TEAM_ID', 'VISITOR_TEAM_ID'
 
 rankings_raw = pd.read_csv(f"{path}/ranking.csv")
 rankings = rankings_raw[['TEAM_ID', 'STANDINGSDATE', 'W_PCT', 'HOME_RECORD', 'ROAD_RECORD', 'W', 'L']]
+
+# Variables to adjust and experiment with
+window_size = 10
+max_tree_depth = 4
+n_est = 100
 
 home_merged = pd.merge(
     games,
@@ -37,9 +39,9 @@ total_merged = pd.merge(
 ).drop(columns=['TEAM_ID_away', 'STANDINGSDATE_away'])
 
 
-# Calculate 'LAST_TEN' variables and merge with existing df.
+# Calculate 'LAST_X' variables and merge with existing df.
 new_test = RDF(games)
-new_test.calc_last_tens()
+new_test.calc_last_xs(window_size)
 
 win_pct = new_test.get_df()
 total_merged = pd.merge(
@@ -58,7 +60,7 @@ total_merged = pd.merge(
     how='left'
 ).drop(columns=['TEAM_ID', 'TEAM_WIN', 'HOME_ROW'])
 
-total_merged = total_merged.rename(columns={'LAST_TEN_x':'LAST_TEN_home', 'LAST_TEN_y':'LAST_TEN_away'})
+total_merged = total_merged.rename(columns={'LAST_X_x':'LAST_X_home', 'LAST_X_y':'LAST_X_away'})
 
 # Converts Home and Road Records into percentages.
 def convert_to_percentage(df, colname, isHomeTeam):
@@ -79,12 +81,12 @@ def convert_to_percentages(data):
     return(df)
     
 total_merged = convert_to_percentages(total_merged)
-print(total_merged.drop(columns='GAME_DATE_EST').head())
+# print(total_merged.drop(columns='GAME_DATE_EST').head())
 
 
-def basic_logistic_regression_w_last_ten(total_merged):
+def basic_logistic_regression_w_last_x(total_merged):
     # Independent (predictors) and Dependent (won?) Columns
-    X = total_merged[['W_PCT_home', 'W_PCT_away', 'HOME_RECORD_home', 'ROAD_RECORD_away', 'LAST_TEN_home', 'LAST_TEN_away']]
+    X = total_merged[['W_PCT_home', 'W_PCT_away', 'HOME_RECORD_home', 'ROAD_RECORD_away', 'LAST_X_home', 'LAST_X_away']]
     y = total_merged['HOME_TEAM_WINS']
 
     X_train, X_test, y_train, y_test = train_test_split(
@@ -97,11 +99,8 @@ def basic_logistic_regression_w_last_ten(total_merged):
     )
 
     model = LogisticRegression()
-    with mlflow.start_run(run_name="logreg_with_last_ten"):
-        model.fit(X_train, y_train)   # autolog captures this call automatically
-        preds = model.predict(X_test)
-
-    print(X.head())
+    model.fit(X_train, y_train)
+    preds = model.predict(X_test)
 
     print(confusion_matrix(y_test, preds))
     print(classification_report(y_test, preds))
@@ -109,9 +108,9 @@ def basic_logistic_regression_w_last_ten(total_merged):
     for feature, coef in zip(X.columns, model.coef_[0]):
         print(f"{feature}: {coef:.3f}")
 
-def decision_tree_w_last_ten(total_merged):
+def decision_tree_w_last_x(total_merged, max_tree_depth):
     # Independent (predictors) and Dependent (won?) Columns
-    X = total_merged[['W_PCT_home', 'W_PCT_away', 'HOME_RECORD_home', 'ROAD_RECORD_away', 'LAST_TEN_home', 'LAST_TEN_away']]
+    X = total_merged[['W_PCT_home', 'W_PCT_away', 'HOME_RECORD_home', 'ROAD_RECORD_away', 'LAST_X_home', 'LAST_X_away']]
     y = total_merged['HOME_TEAM_WINS']
 
     X_train, X_test, y_train, y_test = train_test_split(
@@ -124,10 +123,9 @@ def decision_tree_w_last_ten(total_merged):
     )
 
     # Not as effective since there aren't complex interactions.
-    tree = DecisionTreeClassifier(max_depth=8)
-    with mlflow.start_run(run_name="tree_with_last_ten"):
-            tree.fit(X_train, y_train)   # autolog captures this call automatically
-            tree_preds = tree.predict(X_test)
+    tree = DecisionTreeClassifier(max_depth=max_tree_depth)
+    tree.fit(X_train, y_train)
+    tree_preds = tree.predict(X_test)
 
     print(confusion_matrix(y_test, tree_preds))
     print(classification_report(y_test, tree_preds))
@@ -136,7 +134,7 @@ def decision_tree_w_last_ten(total_merged):
         print(f"{name}: {importance:.3f}")
 
 
-#Without last ten variable
+#Without LAST_X_ variable
 def basic_logistic_regression(total_merged):
     # Independent (predictors) and Dependent (won?) Columns
     X = total_merged[['W_PCT_home', 'W_PCT_away', 'HOME_RECORD_home', 'ROAD_RECORD_away']]
@@ -152,9 +150,8 @@ def basic_logistic_regression(total_merged):
     )
 
     model = LogisticRegression()
-    with mlflow.start_run(run_name="logreg"):
-            model.fit(X_train, y_train)   # autolog captures this call automatically
-            preds = model.predict(X_test)
+    model.fit(X_train, y_train)
+    preds = model.predict(X_test)
 
     print(confusion_matrix(y_test, preds))
     print(classification_report(y_test, preds))
@@ -162,7 +159,7 @@ def basic_logistic_regression(total_merged):
     for feature, coef in zip(X.columns, model.coef_[0]):
         print(f"{feature}: {coef:.3f}")
 
-def decision_tree(total_merged):
+def decision_tree(total_merged, max_tree_depth):
     # Independent (predictors) and Dependent (won?) Columns
     X = total_merged[['W_PCT_home', 'W_PCT_away', 'HOME_RECORD_home', 'ROAD_RECORD_away']]
     y = total_merged['HOME_TEAM_WINS']
@@ -177,10 +174,9 @@ def decision_tree(total_merged):
     )
 
     # Not as effective since there aren't complex interactions.
-    tree = DecisionTreeClassifier(max_depth=8)
-    with mlflow.start_run(run_name="tree"):
-            tree.fit(X_train, y_train)   # autolog captures this call automatically
-            tree_preds = tree.predict(X_test)
+    tree = DecisionTreeClassifier(max_depth=max_tree_depth)
+    tree.fit(X_train, y_train)
+    tree_preds = tree.predict(X_test)
 
     print(confusion_matrix(y_test, tree_preds))
     print(classification_report(y_test, tree_preds))
@@ -188,9 +184,33 @@ def decision_tree(total_merged):
     for name, importance in zip(X.columns, tree.feature_importances_):
             print(f"{name}: {importance:.3f}")
 
+def random_forest(total_merged, n_estimators, max_tree_depth):
+    X = total_merged[['W_PCT_home', 'W_PCT_away', 'HOME_RECORD_home', 'ROAD_RECORD_away']]
+    y = total_merged['HOME_TEAM_WINS']
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size = 0.2,
+        random_state = 38,
+        shuffle = False,
+        stratify = None
+    )
+
+    forest = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_tree_depth)
+    forest.fit(X_train, y_train)
+    forest_preds = forest.predict(X_test)
+
+    print(confusion_matrix(y_test, forest_preds))
+    print(classification_report(y_test, forest_preds))
+
+    # for name, importance in zip(X.columns, forest.feature_importances_):
+    #     print(f"{name}: {importance:.3f}")
+
+# Uncomment to test models.
+
 # basic_logistic_regression(total_merged)
-decision_tree(total_merged)
-# print("With last ten:")
-# basic_logistic_regression_w_last_ten(total_merged)
-decision_tree_w_last_ten(total_merged)
-print(mlflow.get_experiment_by_name("nba_predictor"))
+# decision_tree(total_merged, max_tree_depth)
+# basic_logistic_regression_w_last_x(total_merged)
+# decision_tree_w_last_x(total_merged, max_tree_depth)
+# random_forest(total_merged, n_est, max_tree_depth)
